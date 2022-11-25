02812b2b9de2aa7f990228ea49aee17945c76421
Null <779217162@qq.com>
2022-11-25 10:39:22 +0800

    feat(v1.0.0): 项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
+<!--
+ * @Author: Null
+ * @Date: 2022-11-25 10:16:15
+ * @Description: 
+-->
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
